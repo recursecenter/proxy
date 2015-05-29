0b39dcf1a130838f147df53fafe69acc59fbd0d4
Zachary Allaun <zach.allaun@gmail.com>
2015-05-29 12:15:14 -0400

    Simple logger; STDOUT in dev, syslog in prod. Mock dev? and prod? methods

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,8 +1,36 @@
+require 'logger'
+require 'syslog/logger'
+
 module Proxy
   ROOT = File.expand_path(File.dirname(__FILE__))
 
   require "#{ROOT}/core_extensions/pathname"
 
-  require "#{ROOT}/proxy/nginx_config"
   require "#{ROOT}/proxy/cache"
+  require "#{ROOT}/proxy/nginx_config"
+
+  class << self
+    def production?
+      false
+    end
+
+    def development?
+      true
+    end
+
+    def logger
+      return @logger if defined?(@logger)
+
+      if Proxy.production?
+        @logger = Syslog::Logger.new("proxy")
+        @logger.level = Logger::INFO
+      else
+        @logger = Logger.new(STDOUT)
+        @logger.progname = "proxy"
+        @logger.level = Logger::DEBUG
+      end
+
+      @logger
+    end
+  end
 end
