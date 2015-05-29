97ec314d1683c4746ac2a6ac15dfd372bf720e6b
Zachary Allaun <zach.allaun@gmail.com>
2015-05-29 12:43:28 -0400

    Fix Pathname extension

--- lib/core_extensions/pathname.rb
+++ lib/core_extensions/pathname.rb
@@ -1,23 +1,15 @@
-module CoreExtensions
-  module Pathname
-    # Homebrew inspired hack for pretty-looking paths
-    def /(other)
-      self + other
-    end
-
-    # Backport Ruby 2.1 Pathname#write
-    unless respond_to? :write
-      def write(s)
-        File.open(to_path, "w") do |f|
-          f.write(s)
-        end
-      end
-    end
-  end
-end
-
 require 'pathname'
 
 class Pathname
-  include CoreExtensions::Pathname
+  # Homebrew inspired hack for pretty-looking paths
+  alias / +
+
+  # Backport Ruby 2.1 Pathname#write
+  unless method_defined?(:write)
+    def write(s)
+      File.open(to_path, "w") do |f|
+        f.write(s)
+      end
+    end
+  end
 end
