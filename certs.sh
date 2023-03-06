ceadc22faad7bee4d45341b22bbab8a14384f1d2
David Albert <davidbalbert@gmail.com>
2023-03-06 17:44:03 -0500

    Little bash heroku certs client

--- certs.sh
+++ certs.sh
@@ -5,6 +5,53 @@ set -u # Treat unset variables as an error
 set -f # Disable file globbing
 set -o pipefail # Fail a pipe if any subcommand fails
 
+# To generate HEROKU_API_KEY, run:
+# heroku authorizations:create --scope=read,write --description="..."
+
+function heroku_curl() {
+    curl --silent --fail \
+         --header "Accept: application/vnd.heroku+json; version=3" \
+         --header "Authorization: Bearer $HEROKU_API_KEY" \
+         "$@"
+}
+
+function get_sni_endpoints() {
+    heroku_curl "https://api.heroku.com/apps/$HEROKU_APP_NAME/sni-endpoints"
+}
+
+function endpoint_to_update() {
+    get_sni_endpoints | jq --raw-output --exit-status ".[] | select(.ssl_cert.cert_domains == [\"*.$DOMAIN\"])"
+}
+
+function create_sni_endpoint() {
+    local keyfile="$1"
+    local certfile="$2"
+
+    heroku_curl --request POST "https://api.heroku.com/apps/$HEROKU_APP_NAME/sni-endpoints" \
+                --header "Content-Type: application/json" \
+                --data @<<END
+{
+    "private_key": "$(cat "$keyfile")",
+    "certificate_chain": "$(cat "$certfile")",
+}
+END
+}
+
+function update_sni_endpoint() {
+    local id="$1"
+    local keyfile="$1"
+    local certfile="$2"
+
+    heroku_curl --request PATCH "https://api.heroku.com/apps/$HEROKU_APP_NAME/sni-endpoints/$id" \
+                --header "Content-Type: application/json" \
+                --data @<<END
+{
+    "private_key": "$(cat "$keyfile")",
+    "certificate_chain": "$(cat "$certfile")",
+}
+END
+}
+
 function checkenv() {
     local varname="$1"
 
@@ -15,12 +62,15 @@ function checkenv() {
 }
 
 function request_certificate() {
+    local keyfile="$1"
+    local certfile="$2"
+
     git clone https://github.com/acmesh-official/acme.sh.git
 
     ./acme.sh/acme.sh --install --force --nocron --accountemail "$LETS_ENCRYPT_EMAIL"
     ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 
-    ~/.acme.sh/acme.sh --issue -d "*.$DOMAIN" --dns dns_aws --keylength ec-256 --key-file "/tmp/*.$DOMAIN.key" --fullchain-file "/tmp/*.$DOMAIN.crt" --log
+    ~/.acme.sh/acme.sh --issue -d "*.$DOMAIN" --dns dns_aws --keylength ec-256 --key-file "$keyfile" --fullchain-file "$certfile" --log
 }
 
 if [ "$#" -ne 1 ]; then
@@ -33,15 +83,19 @@ checkenv AWS_ACCESS_KEY_ID
 checkenv AWS_SECRET_ACCESS_KEY
 checkenv LETS_ENCRYPT_EMAIL
 checkenv HEROKU_APP_NAME
+checkenv HEROKU_API_KEY
+
+keyfile="/tmp/*.$DOMAIN.key"
+certfile="/tmp/*.$DOMAIN.crt"
 
 case "$1" in
     issue)
-        request_certificate
-        heroku certs:add "/tmp/*.$DOMAIN.crt" "/tmp/*.$DOMAIN.key" --app "$HEROKU_APP_NAME"
+        request_certificate "$keyfile" "$certfile"
+        create_sni_endpoint "$keyfile" "$certfile"
         ;;
     renew)
         request_certificate
-        heroku certs:update "/tmp/*.$DOMAIN.crt" "/tmp/*.$DOMAIN.key" --app "$HEROKU_APP_NAME" --confirm "$HEROKU_APP_NAME"
+        update_sni_endpoint "$(endpoint_to_update | jq --raw-output '.id')" "$keyfile" "$certfile"
         ;;
     *)
         echo "Usage: $0 [issue|renew]"
